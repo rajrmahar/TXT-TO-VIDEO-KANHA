07c7306bcd03520b691f9922dbeb80e04be8ce62
sonic kuwal <158008154+Soninkuwal@users.noreply.github.com>
2025-02-05 17:59:56 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# ᴛᴜsʜᴀʀ ᴛxᴛ ᴜᴘʟᴏᴀᴅᴇʀ
+By using this repo you can deploy txt uploader bot
---
+++ vars.py
@@ -0,0 +1,8 @@
+# Don't Remove Credit Tg - @Tushar0125
+# Ask Doubt on telegram @Tushar0125
+
+from os import environ
+
+API_ID = int(environ.get("API_ID", "24473318"))
+API_HASH = environ.get("API_HASH", "e7dd0576c5ac0ff8f90971d6bb04c8f5")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
